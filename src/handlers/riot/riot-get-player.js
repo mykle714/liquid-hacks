2c2d902ee4b5c649f7f5f2c7712e5d7c6c24e1e9
Weiming Zhu <35846988+weimzhu@users.noreply.github.com>
2020-11-07 12:43:00 -0500

    trying execution to riot api from lambda

--- src/handlers/riot/riot-get-player.js
+++ src/handlers/riot/riot-get-player.js
@@ -1,19 +1,45 @@
 // Lamda function for retrieving player info
 
+const https = require("https");
+
 exports.getPlayerInfo = async (event) => {
     let { httpMethod, path } = event;
 
     console.log("Received player retrieve request: ", JSON.stringify(event));
 
-    responseBody = {
-        name: "weiming",
-        summonerId: "weimotheninja"
-    }
+    let playerData = new Promise((resolve, reject) => {
+        const options = {
+            host: "na1.api.riotgames.com",
+            path: "/lol/summoner/v4/summoners/by-name/Wuri",
+            method: "GET",
+            headers: {
+                "X-Riot-Token": "RGAPI-fc66061b-f78f-4269-b317-e2449d49d321"
+            }
+        }
 
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify(responseBody)
-    }
+        const request = https.request(options, response => {
+            console.log("response status>>" + res.statusCode);
+            resolve(response)
+        });
 
-    return response
+        request.on("error", error => {
+            console.error(error)
+            reject("Failed")
+        });
+    });
+
+    playerData
+        .then(result => {
+            console.log("got this success from the promise>>" + JSON.stringify(result));
+
+            return result;
+        })
+        .catch(result => {
+            console.log("got this error from the promise>>" + result);
+            errorResponse = {
+                error: "Get player info error"
+            }
+
+            return JSON.stringify(errorResponse);
+        })
 };

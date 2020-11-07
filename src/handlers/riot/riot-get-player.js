80b6655de807bd504ed1a9723a3d915f6be6388b
Weiming Zhu <35846988+weimzhu@users.noreply.github.com>
2020-11-07 13:55:15 -0500

    test http request on lambda

--- src/handlers/riot/riot-get-player.js
+++ src/handlers/riot/riot-get-player.js
@@ -2,25 +2,35 @@
 
 const https = require("https");
 
-exports.getPlayerInfo = async (event) => {
+getPlayerInfo = async (event) => {
     let { httpMethod, path } = event;
 
     console.log("Received player retrieve request: ", JSON.stringify(event));
 
     let playerData = new Promise((resolve, reject) => {
         const options = {
-            host: "na1.api.riotgames.com",
-            path: "/lol/summoner/v4/summoners/by-name/Wuri",
-            method: "GET",
+            // host: "na1.api.riotgames.com",
+            // path: "/lol/summoner/v4/summoners/by-name/Wuri",
+            // method: "GET",
             headers: {
                 "X-Riot-Token": "RGAPI-fc66061b-f78f-4269-b317-e2449d49d321"
             }
         }
 
-        const request = https.request(options, response => {
-            console.log("response status>>" + res.statusCode);
-            resolve(response)
-        });
+        // const request = https.request(options, response => {
+        //     console.log("response status>>" + res.statusCode);
+        //     resolve(response)
+        // });
+
+        const request = https.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Wuri", options, response => {
+            console.log("response status>>" + response.statusCode);
+            console.log("headers>>" + JSON.stringify(response.headers));
+            response.setEncoding('utf8');
+            response.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+                resolve(chunk)
+            });
+        })
 
         request.on("error", error => {
             console.error(error)
